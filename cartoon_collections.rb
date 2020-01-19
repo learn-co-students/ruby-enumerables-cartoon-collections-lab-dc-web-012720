f6f9a4acd7e4032c14088772c7e846bfd4aae1cc
Wesley Chen <weschen1996@gmail.com>
2020-01-19 19:40:17 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each_with_index{|x, index| puts (index + 1 ).to_s + ". "  + x }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  return arr.map{|x| x[0].upcase + x[1, x.length - 1] + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  count = 0
+  while count < arr.length 
+    if arr[count].length > 4
+      return true
+    end
+    count += 1
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(arr)
+  count = 0
+  cheese = %w[cheddar gouda camembert]
+  while count < arr.length
+    if cheese.include? (arr[count])
+      return arr[count]
+    end
+    count += 1
+  end
+  return nil
 end
